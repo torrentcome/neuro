e5c28dfd6af2824fba47f0053a9a8bf3950cf718
torrentcome <torrent.come@gmail.com>
2017-11-14 16:03:13 +0100

    (tonic) import model + readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # neuro
-trying Go language
+
+## Install
+
+
+## Description
+Giving a try on Golang.
+
+model  -> test export
+tictac -> TicTacToe example, complete game
+tonic  -> web service with gin
--- tonic/tonic.go
+++ tonic/tonic.go
@@ -3,6 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	. "../model"
+)
+
+var (
+	db  *gorm.DB
+	err error
 )
 
 func main() {
@@ -21,8 +29,25 @@ func main() {
 	r.Run()
 }
 
+func initDB() {
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&TicTac{})
+}
+
 func getAll(c *gin.Context) {
-	fmt.Print("getAll")
+	var tictac []TicTac
+	if err := db.Find(&tictac).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, tictac)
+	}
+
 	c.JSON(200, gin.H{
 		"message": "Hello World",
 	})
@@ -31,10 +56,12 @@ func getAll(c *gin.Context) {
 func insertOne(c *gin.Context) {
 	fmt.Print("insertOne")
 }
+
 func updateOne(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Printf("updateOne %v", id)
 }
+
 func deleteOne(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Printf("deleteOne %v", id)
